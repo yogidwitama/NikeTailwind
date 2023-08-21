c49056897dd822ebda0113d9f9c0a0397e99a8c5
unknown <yogidwitama56@gmail.com>
2023-08-21 15:59:32 +0700

    setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    <div className="bg-white shadow-xl rounded-xl m-8 py-4 px-2">
+      <h1 className="text-xl font-bold text-gray-700 ml-4">
+        Hello world!
+      </h1>
+      <p className="text-gray-500 ml-4 mt-2">
+        Auto Reload
+      </p>
+    </div>
+
+  )
+}
